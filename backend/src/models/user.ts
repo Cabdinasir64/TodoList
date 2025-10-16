9f5ddf6d8c81adaec109179d5598eabb00ed6da7
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-16 14:11:23 +0300

    add user model with Mongoose schema and interface

---
+++ backend/src/models/user.ts
@@ -0,0 +1,43 @@
+import { Document, Model, model, Schema } from "mongoose";
+
+export interface IUser extends Document {
+    username: string;
+    email: string;
+    password: string;
+}
+
+const UserSchema = new Schema<IUser>(
+    {
+        username: {
+            type: String,
+            required: [true, "Username is required"],
+            trim: true,
+            minlength: 3,
+            maxlength: 50,
+        },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            trim: true,
+            lowercase: true,
+            unique: true,
+            match: [
+                /^\S+@\S+\.\S+$/,
+                "Please provide a valid email address (e.g., user@example.com)",
+            ],
+        },
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+            minlength: 8,
+        },
+    },
+    {
+        strict: true,
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+const User: Model<IUser> = model<IUser>("users", UserSchema);
+export default User;
