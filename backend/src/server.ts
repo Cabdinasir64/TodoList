8f3ac24abd69c7278469fff08c05e43044d65088
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-19 09:02:15 +0300

    chore(server): enable CORS for frontend connection and API access

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,12 +3,20 @@ import dotenv from "dotenv";
 import connectDB from "./mongodb";
 import { limiter } from "./middleware/limiter";
 import userRoutes from "./routes/user";
+import cors from "cors";
 
 dotenv.config();
 connectDB();
 
 
+
 const app = express();
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+}));
+
 app.use(express.json());
 app.use(limiter)
 app.use("/api/users", userRoutes)
