1d358592f0c29985f3576a261a1ebe1a6ee15d0f
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-20 11:16:50 +0300

    feat(homepage): add Hero and Navbar components
    - Imported `Hero` and `Navbar` components on the homepage
    - Rendered `Navbar` at the top and `Hero` below it
    - Updated page metadata for SEO and social sharing

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
-import Hero from './components/hero'
-import Navbar from './components/navbar'
+import Hero from './components/page/hero'
+import Navbar from './components/page/navbar'
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
