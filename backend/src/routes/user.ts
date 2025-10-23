8ec14c3fa0de04210f2f3db28e24f4f9105201d9
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-23 16:28:55 +0300

    added limiter specific where

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createUser, loginUser, logoutUser, getMe } from "../controllers/user";
 import { authMiddleware } from "../middleware/authMiddleware";
+import { limiter } from "../middleware/limiter";
 
 const router = Router();
 
-router.post("/register", createUser);
+router.post("/register", limiter,createUser);
 
-router.post("/login", loginUser);
+router.post("/login", limiter, loginUser);
 
 router.post("/logout", authMiddleware, logoutUser);
 
