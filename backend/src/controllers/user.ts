7c404a9c2c32122227f27d1790f79a65fef3c829
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-26 18:22:40 +0300

    update

--- backend/src/controllers/user.ts
+++ backend/src/controllers/user.ts
@@ -163,7 +163,7 @@ export const loginUser = async (req: Request, res: Response) => {
         res.cookie("token", token, {
             httpOnly: true,
             secure: true,
-            sameSite: "lax",
+            sameSite: "none",
             maxAge: 24 * 60 * 60 * 1000,
         });
 
