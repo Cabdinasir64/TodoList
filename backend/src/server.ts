3b9d35cd87fa26fa853264d5df1bbb3a545a9b0a
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-26 15:53:01 +0300

    i make update origins

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://todo-list-cyan-zeta.vercel.app",
     credentials: true,
 }));
 
