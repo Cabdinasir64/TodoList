f52a61b99e19a0d04712d139b1ef73a07e0b3fd4
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-26 11:59:20 +0300

    feat(user-profile): add admin profile page
    - Created `ProfilePage` to render `UserProfile` component for admin users.
    - Set page metadata with title "Admin Profile | TaskTrek" and description for SEO.

---
+++ frontend/src/app/admin/profile/page.tsx
@@ -0,0 +1,10 @@
+import UserProfile from '../../components/user/profile/UserProfile'
+
+export const metadata = {
+  title: 'Admin Profile | TaskTrek',
+  description: 'Manage your profile information',
+}
+
+export default function ProfilePage() {
+  return <UserProfile />
+}
