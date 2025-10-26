190df4f2ad27bf079fa136223ed753b07eff00c0
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-26 18:41:30 +0300

    Refactor authentication: replace JWT cookies with session-based auth

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,29 +5,45 @@ import userRoutes from "./routes/user";
 import taskRoutes from "./routes/task";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import session from "express-session";
+import MongoStore from "connect-mongo";
 
 dotenv.config();
+
 connectDB();
-
-
-
 
 const app = express();
 
 app.set('trust proxy', 1);
 
 app.use(cookieParser());
+app.use(express.json());
+
+app.use(session({
+    name: "sessionId",
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({
+        mongoUrl: process.env.MONGO_URI!,
+        collectionName: "sessions",
+        ttl: 24 * 60 * 60
+    }),
+    cookie: {
+        httpOnly: true,
+        secure: true,
+        maxAge: 1000 * 60 * 60 * 24,
+        sameSite: "none"
+    }
+}));
 
 app.use(cors({
     origin: "https://todo-list-cyan-zeta.vercel.app",
     credentials: true,
 }));
 
-app.use(express.json());
-app.use("/api/users", userRoutes)
-app.use("/api/tasks", taskRoutes)
+app.use("/api/users", userRoutes);
+app.use("/api/tasks", taskRoutes);
 
-
-
-
-app.listen(process.env.PORT, () => console.log(`🚀 Server running on port ${process.env.PORT}`));
+const PORT = process.env.PORT
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
