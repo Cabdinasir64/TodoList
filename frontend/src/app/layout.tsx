2d91da3de0cc13e1a5bb27be1516580bd5d0dd63
Cabdinasir64 <cabdinasira64@gmail.com>
2025-10-26 15:20:52 +0300

    i make update logo image

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,6 +5,10 @@ import QueryProvider from "./providers/query-provider";
 export const metadata: Metadata = {
   title: "TaskTrek - Organize and Manage Your Tasks Efficiently",
   description: "TaskTrek helps you organize, prioritize, and complete your tasks efficiently.",
+  icons: {
+    icon: "/TaskTrek.ico",
+    shortcut: "/TaskTrek.ico",
+  },
 };
 export default function RootLayout({
   children,
@@ -12,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/tasktrek.png" />
+        <link rel="icon" href="/TaskTrek.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </head>
       <body>
         <QueryProvider>
