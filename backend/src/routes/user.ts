4797884b8d29a77425d8221b40b7744d262921bc
Cabdinasir64 <cabdinasira64@gmail.com>
2025-11-02 13:24:10 +0300

    update

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -2,20 +2,23 @@ import { Router } from "express";
 import {
     createUser, loginUser, logoutUser, getMe, getAdminDashboard, getUsers, updateUserRole
 } from "../controllers/user";
+import { authMiddleware } from "../middleware/authMiddleware";
+import { limiter } from "../middleware/limiter";
+
 const router = Router();
 
-router.post("/register", createUser);
+router.post("/register", limiter, createUser);
 
-router.get("/admin/users", getUsers);
+router.get("/admin/users", authMiddleware, getUsers); 
 
-router.put("/admin/users/:userId/role", updateUserRole);
+router.put("/admin/users/:userId/role", authMiddleware, updateUserRole);
 
-router.post("/login", loginUser);
+router.post("/login", limiter, loginUser);
 
-router.post("/logout", logoutUser);
+router.post("/logout", authMiddleware, logoutUser);
 
-router.get("/me", getMe);
+router.get("/me", authMiddleware, getMe);
 
-router.get("/admin/dashboard", getAdminDashboard);
+router.get("/admin/dashboard", authMiddleware, getAdminDashboard);
 
 export default router;
